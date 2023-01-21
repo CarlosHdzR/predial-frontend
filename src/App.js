e959dd3e25b94695ee62dd907c388306304667e6
chrod <chrod.dev@gmail.com>
2023-01-21 02:27:57 -0500

    Add pre-loader

--- src/App.js
+++ src/App.js
@@ -1,65 +1,81 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./Styles.css";
 import { useAuthContext } from "./context/AuthContext";
 import { Container, Footer, Navbar, Sidebar } from "./components/structure";
 import { systemOutRoutes, adminRoutes, userExtRoutes } from "./routes";
 import { Error404 } from "./pages";
-import { BackToTop } from "./components/minors";
+import { BackToTop, Loader } from "./components/minors";
 import { ToastContainer, Flip } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
   const [hideMenu, setHideMenu] = useState(false); // Toggle-Sidebar
   const { payload, auth } = useAuthContext();
   const role = payload?.role
   const path = (role === 1 || role === 2) ? "/admin/dashboard" : "/user-ext/home";
 
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoaded(true);
+    }, 1000);
+  }, [])
+
   return (
     <>
       <main className={hideMenu ? "toggle-sidebar" : ""}>
-        <Navbar
-          hideMenu={hideMenu}
-          setHideMenu={setHideMenu}
-        />
-        <Sidebar />
-        <Routes>
-          {systemOutRoutes.map((item, index) => (
-            <Route
-              key={index}
-              exact path={item.path}
-              element={!auth
-                ?
-                <Container>{item.element}</Container>
-                :
-                <Navigate to={path} />}
+        {!isLoaded && !auth
+          ?
+          <div className="container-loader">
+            <Loader />
+          </div>
+          :
+          <>
+            <Navbar
+              hideMenu={hideMenu}
+              setHideMenu={setHideMenu}
             />
-          ))}
-          {adminRoutes.map((item, index) => (
-            <Route
-              key={index}
-              path={item.path}
-              element={auth && (role === 1 || role === 2)
-                ?
-                <Container title={item.title} subtitle={item.subtitle}>{item.element}</Container>
-                :
-                <Navigate to="/login" />}
-            />
-          ))}
-          {userExtRoutes.map((item, index) => (
-            <Route
-              key={index}
-              path={item.path}
-              element={auth && role === 3
-                ?
-                <Container title={item.title}>{item.element}</Container>
-                :
-                <Navigate to="/login" />}
-            />
-          ))}
-          <Route path="*" element={<Error404 />} />
-        </Routes>
-        <Footer />
+            <Sidebar />
+            <Routes>
+              {systemOutRoutes.map((item, index) => (
+                <Route
+                  key={index}
+                  exact path={item.path}
+                  element={!auth
+                    ?
+                    <Container>{item.element}</Container>
+                    :
+                    <Navigate to={path} />}
+                />
+              ))}
+              {adminRoutes.map((item, index) => (
+                <Route
+                  key={index}
+                  path={item.path}
+                  element={auth && (role === 1 || role === 2)
+                    ?
+                    <Container title={item.title} subtitle={item.subtitle}>{item.element}</Container>
+                    :
+                    <Navigate to="/login" />}
+                />
+              ))}
+              {userExtRoutes.map((item, index) => (
+                <Route
+                  key={index}
+                  path={item.path}
+                  element={auth && role === 3
+                    ?
+                    <Container title={item.title}>{item.element}</Container>
+                    :
+                    <Navigate to="/login" />}
+                />
+              ))}
+              <Route path="*" element={<Error404 />} />
+            </Routes>
+            <Footer />
+          </>
+        }
       </main>
       <BackToTop />
       <ToastContainer position="top-center" newestOnTop transition={Flip} />
